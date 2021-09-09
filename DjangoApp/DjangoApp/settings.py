a7ee3df71cc3992c22cfa5312d18d4d261d05151
darkpanther99 <kissandor4@gmail.com>
2021-09-09 12:53:15 +0200

    hid the secret key

--- DjangoApp/DjangoApp/settings.py
+++ DjangoApp/DjangoApp/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jf4g!^eo$+q+f=+gcabi3fpkb^%can-b9lr0y&e3_!gcoky*3h'
-
+from DjangoApp.secretsconfig import SECRETS
+SECRET_KEY = SECRETS["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
