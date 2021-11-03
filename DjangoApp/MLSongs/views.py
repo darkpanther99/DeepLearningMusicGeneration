78dcaa756c55eed3416fe36daceb8e889169a2fb
darkpanther99 <kissandor4@gmail.com>
2021-11-03 22:20:24 +0100

    finish signal added to music generation

--- DjangoApp/MLSongs/views.py
+++ DjangoApp/MLSongs/views.py
@@ -22,7 +22,6 @@ def get_random_song(request):
     return render(request, 'song.html', variables)
 
 def model_song(request, model, instrument):
-    print(model)
 
     chosen_song = create_empty_song()
     ML_model_name = ""
@@ -107,6 +106,7 @@ def create_attention(count, instrument):
     input = att.preprocess_data(data)
     att.build_model()
     att.predict(input, count, 0.8)
+    print("Generation task has finished!")
 
 def create_vae(count, instrument_str):
     vae = MusicVAE(instrument_str)
@@ -114,12 +114,14 @@ def create_vae(count, instrument_str):
     data = vae.preprocess_data(data)
     vae.build_model()
     vae.predict(data, count, 0.8)
+    print("Generation task has finished!")
 
 def create_gpt():
     gpt = Music_GPT_2()
     data = gpt.load_data()
     clean_data = gpt.preprocess_data(data)
     gpt.predict(clean_data, -1)
+    print("Generation task has finished!")
 
 def create_multi_lstm(count):
     multi_lstm = MultiInstrumentLSTM()
@@ -127,16 +129,15 @@ def create_multi_lstm(count):
     guitar_input, durations_input, bass_input, drum_input = multi_lstm.preprocess_data(data)
     multi_lstm.build_model()
     multi_lstm.predict(data, count, 0.8)
+    print("Generation task has finished!")
 
 def create_markov(count, instrument_str):
     mc = MarkovModel(instrument_str)
     data = mc.load_data()
     chords, durations = mc.preprocess_data(data)
-    print("Building model")
     mc.build_model(chords, durations)
-    #mc.build_and_save_model(chords, durations)
-    print("Generating music")
     mc.generate_music(chords, durations, count)
+    print("Generation task has finished!")
 
 def create_LSTM(count, instrument):
     lstm = LSTMModel(instrument)
@@ -144,7 +145,7 @@ def create_LSTM(count, instrument):
     input = lstm.preprocess_data(data)
     lstm.build_model()
     lstm.predict(input, count, 0.8)
-
+    print("Generation task has finished!")
 
 def debug(request):
     return HttpResponse('OK')
