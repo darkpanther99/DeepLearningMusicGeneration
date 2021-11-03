99950ed2cc02821bc8b7d4cc1f3cb2041f7a1bf5
darkpanther99 <kissandor4@gmail.com>
2021-11-03 20:23:34 +0100

    config file change

--- DjangoApp/DjangoApp/secretsconfig.py
+++ DjangoApp/DjangoApp/secretsconfig.py
@@ -1,9 +1,9 @@
-
+import os
 
 
 SECRETS = {
-    "DJANGO_SECRET_KEY" :
+    "DJANGO_SECRET_KEY" : "TODO" #insert your secret key here
 }
 
-LOCAL_ABSOLUTE_PATH =
-SOUNDFONT_PATH =
+LOCAL_ABSOLUTE_PATH = os.path.join(os.getcwd(), "training_midis")
+SOUNDFONT_PATH = os.path.join(os.getcwd(), "soundfonts", "default.sf2")
