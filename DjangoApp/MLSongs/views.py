f0f6883c9a8a6ad9e4d87274c25c09e0d6b79fd2
darkpanther99 <kissandor4@gmail.com>
2021-11-11 21:20:27 +0100

    transformer and attention views added

--- DjangoApp/MLSongs/views.py
+++ DjangoApp/MLSongs/views.py
@@ -9,11 +9,15 @@ from MLSongs.ml_agents.multi_instrument_LSTM import MultiInstrumentLSTM
 from MLSongs.database.db_services import get_model, get_songs_by_author, get_all_songs, create_empty_song
 from MLSongs.ml_agents.MusicVAE import MusicVAE
 from MLSongs.ml_agents.Attention import AttentionModel
+from MLSongs.ml_agents.Transformer import TransformerModel
 
 
 def get_random_song(request):
     songs = get_all_songs()
-    chosen_song = random.choice(songs)
+    try:
+        chosen_song = random.choice(songs)
+    except IndexError:
+        return HttpResponse(f"No generated songs found. Execute a model and generate some!")
     variables = {
         'title': chosen_song.title,
         'author': chosen_song.author,
@@ -39,14 +43,23 @@ def model_song(request, model, instrument):
         elif "multi" in instrument.lower():
             ML_model_name = 'LSTMMultiInstrumentModel'
     elif "gpt" in model.lower():
-        ML_model_name = "GPT-2Model"
+        if "guitar" in instrument.lower():
+            ML_model_name = "GPT-2Model"
     elif "vae" in model.lower():
-        ML_model_name = "MusicVAEBass"
+        if "guitar" in instrument.lower():
+            ML_model_name = "MusicVAEGuitar"
+        elif "bass" in instrument.lower():
+            ML_model_name = "MusicVAEBass"
     elif "attention" in model.lower():
         if "guitar" in instrument.lower():
             ML_model_name = "AttentionModel"
         elif "bass" in instrument.lower():
             ML_model_name = "AttentionModelBass"
+    elif "transformer" in model.lower():
+        if "guitar" in instrument.lower():
+            ML_model_name = "TransformerModel"
+        elif "bass" in instrument.lower():
+            ML_model_name = "TransformerModelBass"
 
     ml_author = get_model(ML_model_name)
     if not ml_author:
@@ -54,7 +67,7 @@ def model_song(request, model, instrument):
     try:
         chosen_song = random.choice(get_songs_by_author(ML_model_name))
     except IndexError:
-        return HttpResponse(f"No generated songs found for {ML_model_name}. Execute the model ang generate some!")
+        return HttpResponse(f"No generated songs found for {ML_model_name}. Execute the model and generate some!")
 
 
     variables = {
@@ -64,56 +77,72 @@ def model_song(request, model, instrument):
     }
     return render(request, 'song.html', variables)
 
-def execute_model(request, model, instrument, count):
-    print(model)
+def execute_model(request, model, instrument, count, temp):
 
     if "markov" in model.lower():
-        t = threading.Thread(target=create_markov, args=(count, instrument))
-        t.start()
-        return HttpResponse("Markov Model is working in the background!")
+        if "guitar" in instrument.lower() or "bass" in instrument.lower():
+            t = threading.Thread(target=create_markov, args=(count, instrument, temp))
+            t.start()
+            return HttpResponse("Markov Model is working in the background!")
 
     if "lstm" in model.lower():
         if "guitar" in instrument.lower() or "bass" in instrument.lower():
-            t = threading.Thread(target=create_LSTM, args=(count, instrument))
+            t = threading.Thread(target=create_LSTM, args=(count, instrument, temp))
             t.start()
             return HttpResponse("LSTM is working in the background!")
         elif "multi" in instrument.lower():
-            t = threading.Thread(target=create_multi_lstm, args=(count,))
+            t = threading.Thread(target=create_multi_lstm, args=(count, temp))
             t.start()
             return HttpResponse("LSTM is working in the background!")
 
     if "attention" in model.lower():
         if "guitar" in instrument.lower() or "bass" in instrument.lower():
-            t = threading.Thread(target=create_attention, args=(count, instrument))
+            t = threading.Thread(target=create_attention, args=(count, instrument, temp))
             t.start()
             return HttpResponse("Attention based model is working in the background!")
 
+    if "transformer" in model.lower():
+        if "guitar" in instrument.lower() or "bass" in instrument.lower():
+            t = threading.Thread(target=create_transformer, args=(count, instrument, temp))
+            t.start()
+            return HttpResponse("Transformer based model is working in the background!")
+
     if "gpt" in model.lower():
-        t = threading.Thread(target=create_gpt)
-        t.start()
-        return HttpResponse("GPT-2 postprocessor is working in the background!")
+        if "guitar" in instrument.lower():
+            t = threading.Thread(target=create_gpt)
+            t.start()
+            return HttpResponse("GPT-2 postprocessor is working in the background!")
 
     if "vae" in model.lower():
-        t = threading.Thread(target=create_vae, args=(count, instrument))
-        t.start()
-        return HttpResponse("MusicVAE is working in the background!")
+        if "guitar" in instrument.lower() or "bass" in instrument.lower():
+            t = threading.Thread(target=create_vae, args=(count, instrument, temp))
+            t.start()
+            return HttpResponse("MusicVAE is working in the background!")
 
-    return HttpResponse("OK")
+    return HttpResponse("No model found with that name and instrument combination!")
 
-def create_attention(count, instrument):
-    att = AttentionModel(instrument)
-    data = att.load_data()
-    input = att.preprocess_data(data)
-    att.build_model()
-    att.predict(input, count, 0.8)
+def create_transformer(count, instrument, temp):
+    transformer = TransformerModel(instrument)
+    data = transformer.load_data()
+    data = transformer.preprocess_data(data)
+    transformer.build_model()
+    transformer.predict(data, count, temp)
     print("Generation task has finished!")
 
-def create_vae(count, instrument_str):
+def create_attention(count, instrument, temp):
+    att = AttentionModel(instrument)
+    data = att.load_data()
+    data = att.preprocess_data(data)
+    att.build_model()
+    att.predict(data, count, temp)
+    print("Generation task has finished!")
+
+def create_vae(count, instrument_str, temp):
     vae = MusicVAE(instrument_str)
     data = vae.load_data()
     data = vae.preprocess_data(data)
     vae.build_model()
-    vae.predict(data, count, 0.8)
+    vae.predict(data, count, temp)
     print("Generation task has finished!")
 
 def create_gpt():
@@ -123,12 +152,12 @@ def create_gpt():
     gpt.predict(clean_data, -1)
     print("Generation task has finished!")
 
-def create_multi_lstm(count):
+def create_multi_lstm(count, temp):
     multi_lstm = MultiInstrumentLSTM()
     data = multi_lstm.load_data()
     guitar_input, durations_input, bass_input, drum_input = multi_lstm.preprocess_data(data)
     multi_lstm.build_model()
-    multi_lstm.predict(data, count, 0.8)
+    multi_lstm.predict(data, count, temp)
     print("Generation task has finished!")
 
 def create_markov(count, instrument_str):
@@ -139,19 +168,23 @@ def create_markov(count, instrument_str):
     mc.generate_music(chords, durations, count)
     print("Generation task has finished!")
 
-def create_LSTM(count, instrument):
+def create_LSTM(count, instrument, temp):
     lstm = LSTMModel(instrument)
     data = lstm.load_data()
-    input = lstm.preprocess_data(data)
+    data = lstm.preprocess_data(data)
     lstm.build_model()
-    lstm.predict(input, count, 0.8)
+    lstm.predict(data, count, temp)
     print("Generation task has finished!")
 
 def debug(request):
+
     return HttpResponse('OK')
 
 def execute_model_once(request, model, instrument):
-    return execute_model(request, model, instrument, 1)
+    return execute_model_without_temp(request, model, instrument, 1)
+
+def execute_model_without_temp(request, model, instrument, count):
+    return execute_model(request, model, instrument, count, 0.8)
 
 def help(request):
     return render(request, 'help.html')
